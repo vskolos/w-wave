66f5a6af8143eb608e481c48a3d0b0a6044fe849
Vsevolod Kolos <vskolos@icloud.com>
2022-05-03 00:37:12 +0300

    feat: guests section complete

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 (() => {
 
   document.addEventListener('DOMContentLoaded', () => {
+
+    // Authors selector
     const authors = new Choices('.shows__authors', {
       allowHTML: false,
       searchEnabled: false,
@@ -9,13 +11,39 @@
       itemSelectText: '',
     });
 
-    const guests = new Accordion('.guests__types', {
+    // Guests types accordion
+    const guestTypes = new Accordion('.guests__types', {
+      duration: 300,
       elementClass: 'guests__type',
       triggerClass: 'guests__type-wrapper',
       panelClass: 'guests__type-panel',
       activeClass: 'guests__type--is-active',
       openOnInit: [0],
     });
+
+    // Chosen guest info
+    const guests = document.querySelectorAll('.guests__btn');
+    guests.forEach(guest => {
+      guest.addEventListener('click', e => {
+        const path = e.currentTarget.dataset.path;
+
+        guests.forEach(link => {
+          link.classList.remove('guests__btn--active');
+        });
+        e.currentTarget.classList.add('guests__btn--active');
+
+        document.querySelectorAll('.guest').forEach(div => {
+          div.classList.remove('guest--active');
+        })
+
+        const target = document.querySelector(`[data-target="${path}"]`);
+        if (target) {
+          target.classList.add('guest--active');
+        } else {
+          document.querySelector(`[data-target="nobody"]`).classList.add('guest--active');
+        }
+      })
+    })
   });
 
 })();
